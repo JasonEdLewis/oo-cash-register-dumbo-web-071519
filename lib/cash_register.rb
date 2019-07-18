676138a435aa50ae4974a57fef4b511ce6b63742
Jason Lewis <jasonedlewis@gmail.com>
2019-07-18 23:41:07 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -4,8 +4,11 @@ class CashRegister
   def initialize(total=0)
     @total = total
     @employee_discount
-    
+  
   end
   
+  def add_item(item,price,quantity)
+    @total += price
+  end
   
 end
