6c3cfce3bca77be0a4678d4a8e723c304a757355
Jason Lewis <jasonedlewis@gmail.com>
2019-07-18 23:53:18 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -2,7 +2,8 @@ class CashRegister
   attr_accessor :total, :discount
   
   def initialize(total=0, discount = 0)
-    @total = total - (total*discount)
+    @total = total
+    @discount = discount
     
   end
   
