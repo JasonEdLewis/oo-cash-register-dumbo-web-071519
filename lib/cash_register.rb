7582babc10144250a06ce3ccbced3e2ab7aaced5
Jason Lewis <jasonedlewis@gmail.com>
2019-07-19 00:12:19 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -11,7 +11,7 @@ class CashRegister
   def add_item(item,price, amount = 1)
     @total += price*amount
     previous_total = @total - price
-    @items.push((item))
+    @items.push(item)*amount
   end
   
   def apply_discount
